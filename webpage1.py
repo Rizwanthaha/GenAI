f8ef1bd4df1fb2e359345145fe5f93ce21ce688a
Rizwanthaha <rizwanthaha2005@gmail.com>
2025-11-22 23:08:43 +0530

    Refactor chatbot interface and response handling

--- webpage1.py
+++ webpage1.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from google import genai
-st.title("My chatGPT")
-chitti=genai.Client(api_key="AIzaSyCetBQ5D_MU5WpG_9ftQvDTmn-I8B7B74c")
-question=st.text_input("Ask Anything")
+
+bot = genai.Client(api_key = "AIzaSyCetBQ5D_MU5WpG_9ftQvDTmn-I8B7B74c")
+st.title("Rizwan's GPT")
+
+query = st.text_input("Ask Anything : ")
+response = bot.models.generate_content(model = "gemini-2.5-flash", contents = query)
 
 if st.button("Send"):
-    response=chitti.models.generate_content(
-        model="gemini-2.5-flash",
-               contents=question)
-    st.write(response.text)
+    st.write("Bot : " + response.text)
